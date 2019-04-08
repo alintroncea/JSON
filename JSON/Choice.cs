4ad0276e13e9cd390074944e645a17dd403230e3
alin.troncea <alintroncea@gmail.com>
2019-04-08 18:00:46 +0300

    first

--- JSON/Choice.cs
+++ JSON/Choice.cs
@@ -19,7 +19,7 @@ namespace JSON
         {
             foreach(var pattern in patterns)
             {
-               
+               int
                         
             }
             return matchClass;
